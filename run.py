f49739cbd7def29df771fe09ea16890578882b73
chrisgustafsson <>
2023-11-03 17:11:53 +0000

    FIX issues with the steps

--- run.py
+++ run.py
@@ -29,20 +29,20 @@ def play_game(word):
     guess = False
     guess_letters = []
     guess_words = []
-    steps = 6
+    steps = 0
     print("\n".join(display_title()))
     print("\n".join(intro()))
     print(display_hangman(steps))
     print(word_completion)
     print("\n")
-    while not guess and steps > 0:
+    while not guess and steps < 6:
         guesses = input("Make a guess of a letter or word:\n").upper()
         if len(guesses) == 1 and guesses.isalpha():
             if guesses in guess_letters:
                 print("You already guessed this letter silly", guesses)
             elif guesses not in word:
                 print(guesses, "This letter is not in the word. Try again!")
-                steps -= 1
+                steps += 1
                 guess_letters.append(guesses)
             else:
                 print("Amazing, well done", guesses, "is there!")
@@ -59,7 +59,7 @@ def play_game(word):
                 print("You already guessed the word silly", guesses)
             elif guesses != word:
                 print(guesses, "is not the word we look for!")
-                steps -= 1
+                steps += 1
                 guess_words.append(guesses)
             else:
                 guess = True
