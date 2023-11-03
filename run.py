20c06103d0f29dd4a522d97b9a68fc5938fbb040
chrisgustafsson <>
2023-11-03 16:37:48 +0000

    UPDATE main function with play again possibility

--- run.py
+++ run.py
@@ -93,3 +93,8 @@ def display_hangman(steps):
 def main():
     word = get_word()
     play_game(word)
+    while input ("Would you like to play again? (yes/no) ").upper() == "YES":
+        word = get_word()
+        play_game(word)
+
+
