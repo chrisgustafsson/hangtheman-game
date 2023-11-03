1d8572e3d86643b68bda827c90e8342ef0ec8287
chrisgustafsson <>
2023-11-03 15:18:10 +0000

    ADD first section of game functionality

--- run.py
+++ run.py
@@ -14,3 +14,15 @@ def display_title():
 def get_word():
     word = random.choice(words)
     return word.upper()
+
+
+def play_game(word):
+    word_completion = "_" * len(word)
+    guess = False
+    guess_letters = []
+    guess_words = []
+    steps = 6
+    print(display_title)
+    print(intro)
+    print(word_completion)
+    print("\n")
